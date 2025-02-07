e7b7ef49d4500c857551a6888c74dcdfec3ae8f4
Gurmeet <gurmeetrajsnm@gmail.com>
2025-02-07 21:12:30 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import getImageUrl from "../../../assets/about/pic.jpg";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Gurmeet</h1>
+        <p className={styles.description}>
+          I am a full-stack developer specializing in React.js, Node.js,
+          Express.js, and MongoDB, building scalable, user-centric, and
+          high-performance web applications. Skilled in optimization,
+          responsiveness, and problem-solving, I excel in collaborative
+          environments to deliver quality projects on time.
+        </p>
+        <a href="mailto:gurmeetrajsnm@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
