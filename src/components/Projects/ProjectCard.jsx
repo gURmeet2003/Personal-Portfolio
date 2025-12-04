b9d80cee68a3c3ec493864e002ac182223a77b2a
gURmeet2003 <gurmeetrajsnm@gmail.com>
2025-12-04 12:23:28 +0530

    final commit

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -15,19 +15,8 @@ export const ProjectCard = ({
       />
       <h3 className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
-      <ul className={styles.skills}>
-        {skills.map((skill, id) => {
-          return (
-            <li key={id} className={styles.skill}>
-              {skill}
-            </li>
-          );
-        })}
-      </ul>
+
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
-          Demo
-        </a>
         <a href={source} className={styles.link}>
           Source
         </a>
